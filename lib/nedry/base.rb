cec85089614153a619737caadf2e4c04c9e0bdf2
Kyle Drake <kyledrake@gmail.com>
2011-11-12 09:08:09 +0100

    Test 404, xml and generic 500

--- lib/nedry/base.rb
+++ lib/nedry/base.rb
@@ -3,22 +3,18 @@ require 'xmlsimple'
 MultiJson.engine = :yajl
 module Nedry
   class Base
-    
+
     @@routes = []
 
     class << self
       @routes = []
       def get(path, &block); add_route('GET', path, &block) end
       def post(path, &block)
-        add_route 
-      end
-    
-      def add_route(request_method, path, &block)
-        @@routes << {:request_method => request_method, :path => path, :block => block}
+        add_route
       end
 
-      def error(status, type, message)
-        {:error => {:type => type, :message => message}}
+      def add_route(request_method, path, &block)
+        @@routes << {:request_method => request_method, :path => path, :block => block}
       end
 
       def match(request_method, path)
@@ -31,58 +27,87 @@ module Nedry
       end
 
       def call(env)
-        self.new.call! env
+        begin
+          self.new.call env
+        rescue => e
+
+        end
       end
     end
-    
-    
+
+
+    def request_method
+      @env['REQUEST_METHOD']
+    end
+
+    def path
+      @env['PATH_INFO']
+    end
+
+    def params
+      @params ||= Rack::Utils.parse_query @env['QUERY_STRING']
+    end
+
+    def call(env)
+      begin
+        call! env
+      rescue
+        @response = error 500, 'server_error', 'An unexpected error has occured, please try your request again later.'
+        render
+        rack_response
+      end
+    end
+
     def call!(env)
-      request_method = env['REQUEST_METHOD']
-      path = env['PATH_INFO']
-      params = Rack::Utils.parse_query env['QUERY_STRING']
-      headers = {}
-    
+      @env = env
+      @headers = {}
+
       id_match = path.match(/\/:(\w+)/)
       id = id_match.nil? ? nil : id_match.captures.first
-    
+
       format_match = path.match(/.(\w+)$/)
-      format = format_match.nil? ? nil : format_match.captures.first
-    
+      @format = format_match.nil? ? 'json' : format_match.captures.first
+
       route = self.class.match request_method, path
-    
+
       if route.nil?
-        response = error(404, 'not_found', 'The requested method was not found') if route.nil?
+        @response = error(404, 'not_found', 'The requested method was not found') if route.nil?
       else
-        response = route[:block].call env, params
+        @response = route[:block].call @env, params
       end
 
-      if (response.is_a?(Hash) && !response[:error]) || !response.is_a?(Hash)
-        response = {:response => response}
+      if (@response.is_a?(Hash) && !@response[:error]) || !@response.is_a?(Hash)
+        @response = {:response => @response}
       end
 
-      response = render(format)
-    
-      headers['Content-Type'] = 'text/plain' if params['browser_debug']
-    
-      headers['Content-Length'] = response.length.to_s      
-      [200, headers, [response]]
+      render
+      rack_response
     end
-    
-    
+
+    def rack_response
+      @headers['Content-Type'] = 'text/plain' if params['browser_debug']
+      @headers['Content-Length'] = @response.length.to_s
+      [200, @headers, [@response]]
+    end
+
+    def error(status, type, message)
+      {:error => {:type => type, :message => message}}
+    end
+
     private
-    
-    def render(format)
-      if format == 'xml'
-        response = XmlSimple.xml_out response, 
+
+    def render
+      if @format == 'xml'
+        @response = XmlSimple.xml_out @response,
                                      'NoAttr' => true,
                                      'KeepRoot' => true,
                                      'XmlDeclaration' => '<?xml version="1.0" encoding="UTF-8"?>'
-        headers['Content-Type'] = 'application/xml'
+        @headers['Content-Type'] = 'application/xml'
       else
-        response = MultiJson.encode response
-        headers['Content-Type'] = 'application/json'
+        @response = MultiJson.encode @response
+        @headers['Content-Type'] = 'application/json'
       end
     end
-    
+
   end
 end
--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -2,7 +2,7 @@ require_relative './helper.rb'
 
 describe 'A mock app' do
   include Rack::Test::Methods
-  it 'successfully completes a sleep call' do
+  it 'does basic return' do
     mock_app {
       get '/' do
         'ok'
@@ -12,5 +12,51 @@ describe 'A mock app' do
     
     expect_that { last_response.ok? }
     expect_that { last_response.body == {:response => 'ok'}.to_json }
+  end
+  
+  it 'returns get with json' do
+    mock_app {
+      get '/systems.json' do
+        [{:name => 'Main Security Grid'}]
+      end
+    }
+    get '/systems.json'
+    
+    expect_that { last_response.ok? }
+    expect_that { last_response.body == {:response => [{:name => 'Main Security Grid'}]}.to_json }
+  end
+  
+  it 'returns get with xml' do
+    mock_app {
+      get '/systems.xml' do
+        [{:name => 'Main Security Grid'}]
+      end
+    }
+    get '/systems.xml'
+    
+    expect_that { last_response.ok? }
+    expect_that { last_response.body == "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<response>\n  <name>Main Security Grid</name>\n</response>\n" }
+  end
+  
+  it 'returns 404 with json' do
+    mock_app {}
+    get '/access_main_security_grid.json'
+    
+    expect_that { last_response.ok? }
+    expect_that { last_response.body == {:error => {:type => 'not_found', :message => 'The requested method was not found'}}.to_json }
+  end
+  
+  it 'returns 500 on generic error' do
+    mock_app {
+      get '/error' do
+        raise 'HA HA HA'
+      end
+    }
+    get '/error'
+    
+    expect_that { last_response.ok? }
+    expect_that { last_response.body == 
+                  {:error => {:type => 'server_error', 
+                              :message => 'An unexpected error has occured, please try your request again later.'}}.to_json }
   end
 end
